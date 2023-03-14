9fce500cd1dbd80fe2d4ca1aa52dcd934ca4278c
kngsong1 <kristinagiannoutsos@gmail.com>
2023-03-14 18:14:24 -0500

    working on login continue, changed where Routes were, added Protect file and CreateAccount file, similar to Thomas's last project

---
+++ src/components/Protected.js
@@ -0,0 +1,39 @@
+import { React, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { logIn,setUserAccount } from '../features/swapSlice';
+
+export const Protected= ({isLoggedIn, children}) => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        async function checkAuth() {
+            await fetch("/check-auth")
+                .then(response => response.json())
+                .then((json) => {
+                    if(json.isLoggedIn) {
+                        dispatch(logIn());
+                        dispatch(setUserAccount(json.username));
+                    } else {
+                        navigate("/login");
+                    }
+                    
+            });
+        }
+        if (!isLoggedIn) {
+           checkAuth();
+        }
+    })
+
+    if (!isLoggedIn) {
+        console.log("Not logged in");
+
+    } else {
+        return (
+            <>
+                {children}
+            </>
+        )
+    }    
+}
