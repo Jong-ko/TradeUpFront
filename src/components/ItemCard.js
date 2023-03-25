0571503c3c991ce1833e44cb58f22b5c3e8aa943
Jeremy Ongko <ongkojeremy@gmail.com>
2023-03-24 23:48:00 -0400

    included name in itemcard

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -14,13 +14,14 @@ function ItemCard(props) {
         >
           <div className="bg-fixed transition duration-300 ease-in-out hover:opacity-50">
             <div className="rounded shadow-none transition-shadow duration-700 ease-in-out hover:shadow-lg hover:shadow-black/80">
-              <img
+              {/* <img
                 src={"http://localhost:3001/images/" + props.itemInfo.image}
                 alt="not found"
                 className="cursor-pointer rounded-lg bg-white p-1 dark:border-neutral-700 w-full h-48 object-cover"
-              />
+              /> */}
             </div>
           </div>
+          <p>{props.itemInfo.name}</p>
           <p>{props.itemInfo.description}</p>
           <p>{props.itemInfo.category}</p>
           {myItem[0] && <TradeButton itemInfo={props.itemInfo} />}
